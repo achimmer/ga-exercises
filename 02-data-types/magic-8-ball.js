e19dca8eceabf08894bc1c5fa220711f8921c2d4
Andrew Himmer <achimmer@gmail.com>
2016-06-30 18:48:45 -0500

    updated selection variable

--- 02-data-types/magic-8-ball.js
+++ 02-data-types/magic-8-ball.js
@@ -1,3 +1,3 @@
 var options = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes, definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', 'Dont count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful'];
-var selection = Math.floor(Math.random() * 19);
+var selection = Math.floor(Math.random() * options.length, 0);
 console.log('Magic 8 Ball says:', '#', selection, options[selection]);
