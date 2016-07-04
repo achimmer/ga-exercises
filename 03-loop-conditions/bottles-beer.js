d3b77bd5e6bd751afd6757ad32917cee69cf8397
Andrew Himmer <achimmer@gmail.com>
2016-07-04 15:53:41 -0500

    bottles of beer HW assign

---
+++ 03-loop-conditions/bottles-beer.js
@@ -0,0 +1,9 @@
+var beers = 99;
+while(beers > 0) {
+  if (beers > 1) {
+    console.log(beers, 'bottles of beer on the wall', beers, 'bottles of beer, take one down pass it around', beers, 'bottles of beer on the wall');
+  } else {
+    console.log(beers, 'bottle of beer on the wall', beers, 'bottle of beer, take one down pass it around', beers, 'bottle of beer on the wall');
+  }
+  beers--;
+}
