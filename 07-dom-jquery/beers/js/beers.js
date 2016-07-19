921e3afdb7232638ef18e78ae1702b08e6e17fa2
Andrew Himmer <achimmer@gmail.com>
2016-07-18 21:48:14 -0500

    stuff changed

--- 07-dom-jquery/beers/js/beers.js
+++ 07-dom-jquery/beers/js/beers.js
@@ -4,28 +4,28 @@ $(function() {
   var $songList = $('#song-list');
 
   $startBtn.on('click', function() {
-    var listItem = '<li>' + $beerSong.val() + '</li>'
-    $songList.append(listItem);
-  });
+    var listItem = '<li>' + $beerBallad.val() + '</li>';
 
-  function beerSong() {
-    var totalBeers = 99;
-    for(var numBeers = totalBeers; numBeers >= 0; numBeers--) {
-      var amtOfBeers = function(numBeers) {
-        if(numBeers > 1 || numBeers === 0) {
-          return numBeers + ' bottles';
+    function beerBallad() {
+      var totalBeers = 99;
+      for(var numBeers = totalBeers; numBeers >= 0; numBeers--) {
+        var amtOfBeers = function(numBeers) {
+          if(numBeers > 1 || numBeers === 0) {
+            return numBeers + ' bottles';
+          } else {
+            return numBeers + ' bottle';
+          }
+        };
+        var beerSong = '\n' + amtOfBeers(numBeers) + ' of beer on the wall, ' + amtOfBeers(numBeers) + ' of beer. Take one down and pass it around, ' + amtOfBeers(numBeers - 1) + ' of beer on the wall.';
+        if(numBeers >= 1) {
+          console.log(beerSong);
         } else {
-          return numBeers + ' bottle';
+          console.log('\nNo more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, ' + totalBeers.toString() + ' bottles of beer on the wall.');
         }
-      };
-
-      var beerSong = '\n' + amtOfBeers(numBeers) + ' of beer on the wall, ' + amtOfBeers(numBeers) + ' of beer. Take one down and pass it around, ' + amtOfBeers(numBeers - 1) + ' of beer on the wall.';
-
-      if(numBeers >= 1) {
-        console.log(beerSong);
-      } else {
-        console.log('\nNo more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, ' + totalBeers.toString() + ' bottles of beer on the wall.');
       }
+
+      $songList.append(listItem);
+
     }
-  }
+  });
 });
